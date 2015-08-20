f5816be844f52bc40fff397731a47be8df21495b
lievi <lievi.silva@gmail.com>
2015-08-20 18:28:45 -0300

    Teste de incusão dos tópicos do menu vertical via banco. O teste consiste em realizar a mesma operação do usuarios.index no home.index.
    Necessário verificar como criar um controller para o layout, ou mover o menu para o home.index.

--- controllers/home.js
+++ controllers/home.js
@@ -1,9 +1,19 @@
+	var moment = require('moment');
 	module.exports = function(app){
+		
+		var Usuario = app.models.usuarios;
 
 		var HomeController = {
-			index: function(req,res){
-				res.render('home/index');
-			}
+			index:function(req,res){
+				Usuario.find(function(err,data){
+					if(err){
+						console.log(err);
+					}
+					res.render('home/index',{lista:data, moment:moment});
+				});				
+			},
 		}
 		return HomeController;
 	}
+	
+	
