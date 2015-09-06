13cc8993903339356c5c972696609b3519d0f67a
lievi <lievi.silva@gmail.com>
2015-09-06 19:17:02 -0300

    menu funcionando

--- controllers/home.js
+++ controllers/home.js
@@ -2,13 +2,23 @@
 	module.exports = function(app){
 		
 		var Pergunta = app.models.pergunta;
+		var Linguagem = app.models.linguagem;
 		var HomeController = {
-			index:function(req,res){	
-				Pergunta.find({linguagem:req.params.ling},function(err,data){
-					if(err){
+			index:function(req,res){				
+				Pergunta.find({linguagem:req.params.ling},function(err,perguntas){
+					if(err)
+					{
 						console.log(err);
 					}
-					res.render('home/index',{lista:data, moment:moment});
+					
+					else
+					{
+						//Criando outra função dentro da primeira função que está chamando o banco de linguagens
+						Linguagem.find(function(err,linguagens) 
+						{
+							res.render('home/index',{perguntas:perguntas, moment:moment, linguagens:linguagens});												
+						});
+					}
 				}).sort({_id:-1});
 			},
 		}
